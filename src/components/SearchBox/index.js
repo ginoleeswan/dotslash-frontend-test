098d1296570ee409fb16412ee2a826c78dbbe70f
Gino Swanepoel <ginoleemusic@live.com>
2021-11-20 19:10:54 +0200

    styled searchbox

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,12 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import "@pathofdev/react-tag-input/build/index.css";
+import ReactTagInput from "@pathofdev/react-tag-input";
 // import { Range, Tooltip, Handle } from "rc-slider";
 import "rc-slider/assets/index.css";
 
 const SearchBox = () => {
   const [pricing, setPricing] = useState([300, 2000]);
+  const [tags, setTags] = useState(["example tag"]);
 
   // const handle = () => {
   //   <Tooltip
@@ -26,22 +29,50 @@ const SearchBox = () => {
 
   return (
     <Container className="searchBoxContainer">
-      <Row className="searchBoxHeader"></Row>
+      <Row className="searchBoxHeader">
+        <Row className="sbHeader1 align-items-center justify-content-center">
+          <p className="pLight">
+            <FontAwesomeIcon
+              className="searchBoxTitleBackIcon"
+              icon={faChevronLeft}
+            />
+            Hide
+          </p>
+        </Row>
+        <Row className="sbHeader2 align-items-center justify-content-center">
+          <h4 className="searchBoxTitle">
+            Search Reway{" "}
+            <FontAwesomeIcon className="searchBoxTitleIcon" icon={faSearch} />
+          </h4>
+        </Row>
+      </Row>
       <Form className="searchBoxFormContainer">
-        <Form.Group as={Row} className="mb-3" controlId="formHorizontalBrands">
+        <Form.Group as={Row} className="mb-4" controlId="formHorizontalBrands">
           <Form.Label sm={2}>
             <h6>brands</h6>
           </Form.Label>
-          <Form.Control type="email" placeholder="Email" />
+          <ReactTagInput
+            tags={tags}
+            onChange={(newTags) => setTags(newTags)}
+            placeholder=" "
+          />
         </Form.Group>
 
-        <Form.Group as={Row} className="mb-3" controlId="formHorizontalColor">
+        <Form.Group as={Row} className="mb-4" controlId="formHorizontalColor">
           <Form.Label>
             <h6>color</h6>
           </Form.Label>
+          <Form.Select aria-label="Default select example">
+            <option>SELECT COLOR</option>
+            <option value="1">Brown</option>
+            <option value="2">Black</option>
+            <option value="3">White</option>
+            <option value="4">Red</option>
+            <option value="5">Blue</option>
+          </Form.Select>
         </Form.Group>
 
-        <Form.Group as={Row} className="mb-3" controlId="formHorizontalPricing">
+        <Form.Group as={Row} className="mb-5" controlId="formHorizontalPricing">
           <Form.Label>
             <h6>pricing</h6>
           </Form.Label>
