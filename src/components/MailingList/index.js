6bfb6fe1ab41e7a767ebee1eaaebf87cc0235af1
Gino Swanepoel <ginoleemusic@live.com>
2021-11-20 20:25:26 +0200

    styling

--- src/components/MailingList/index.js
+++ src/components/MailingList/index.js
@@ -1,8 +1,4 @@
-import {
-  faSearch,
-  faEnvelope,
-  faTimes,
-} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Container, Form, Row, Button } from "react-bootstrap";
