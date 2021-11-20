931322e515f356ec6752d853846bcf2353095ce1
Gino Swanepoel <ginoleemusic@live.com>
2021-11-20 20:26:17 +0200

    fixed spacing

--- src/components/SubHeader/index.js
+++ src/components/SubHeader/index.js
@@ -5,18 +5,22 @@ const SubHeader = () => {
   return (
     <Container className="sub-header ">
       <Row className="align-items-center justify-content-center h-100">
-        <Col sm={8}>
+        <Col sm={7}>
           <div class="text-center">Adventure is just around the corner!</div>
         </Col>
         <Col sm={2}>
-          <Button className="btn-outline" variant="outline-primary">
-            Find a Store
-          </Button>
+          <Row className="align-items-center justify-content-center">
+            <Button className="btn-outline" variant="outline-primary">
+              Find a Store
+            </Button>
+          </Row>
         </Col>
-        <Col sm={2}>
-          <a href="/#" className="grey-link text-center">
-            Not in South Africa?
-          </a>
+        <Col sm={3}>
+          <Row className="align-items-center justify-content-center">
+            <a href="/#" className="grey-link text-center">
+              Not in South Africa?
+            </a>
+          </Row>
         </Col>
       </Row>
     </Container>
