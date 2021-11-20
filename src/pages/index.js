9b01ff15e067a3f610872eaa99e90d221c199504
Gino Swanepoel <ginoleemusic@live.com>
2021-11-20 20:26:23 +0200

    added new components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,68 @@
 import React from "react";
 import { Button, Container, Row, Col, Breadcrumb } from "react-bootstrap";
 import Advert from "../components/Advert";
+import Footer from "../components/Footer";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/Header";
 import MailingList from "../components/MailingList";
+import ProductCarousel from "../components/ProductCarousel";
+import ProductInfo from "../components/ProductInfo";
+import ProductPurchaseForm from "../components/ProductPurchaseForm";
 import SearchBox from "../components/SearchBox";
 import SubHeader from "../components/SubHeader";
 import TopBanner from "../components/TopBanner";
 
 const Home = () => {
   return (
-    <Container>
+    <Container className="mainContainer">
       <Header />
       <Row>
         <SubHeader />
       </Row>
+      <Container className="mainContentContainer">
+        <Row>
+          <Col className="sideContainer" sm>
+            <Row>
+              <SearchBox />
+            </Row>
+            <Row>
+              <Advert />
+            </Row>
+            <Row>
+              <MailingList />
+            </Row>
+          </Col>
 
-      <Row>
-        <Col className="sideContainer" sm>
-          <Row>
-            <SearchBox />
-          </Row>
-          <Row>
-            <Advert />
-          </Row>
-          <Row>
-            <MailingList />
-          </Row>
-        </Col>
-
-        <Col className="infoContainer" lg>
-          <Row>
-            <TopBanner />
-            <Breadcrumb>
-              <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-              <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
-                Boots
-              </Breadcrumb.Item>
-              <Breadcrumb.Item active>Dylan Hiker Boots</Breadcrumb.Item>
-            </Breadcrumb>
-          </Row>
-        </Col>
-      </Row>
-      {/* <Container>
-        <Button>Test Button</Button>
-      </Container> */}
+          <Col className="infoContainer" lg>
+            <Row>
+              <TopBanner />
+              <Breadcrumb>
+                <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+                <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+                  Boots
+                </Breadcrumb.Item>
+                <Breadcrumb.Item active>Dylan Hiker Boots</Breadcrumb.Item>
+              </Breadcrumb>
+            </Row>
+            <Row>
+              <Col>
+                <ProductCarousel />
+              </Col>
+              <Col>
+                <Row>
+                  <ProductInfo />
+                </Row>
+                <Row>
+                  <ProductPurchaseForm />
+                </Row>
+              </Col>
+            </Row>
+            <Row>more from boots</Row>
+            <Row>boots list</Row>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
     </Container>
   );
 };
