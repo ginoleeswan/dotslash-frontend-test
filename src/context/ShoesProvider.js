82f081a41b0d7829e3c90163cef6bc1c6b4e18ef
Gino Swanepoel <ginoleemusic@live.com>
2021-11-22 01:19:00 +0200

    implemented images properly

--- src/context/ShoesProvider.js
+++ src/context/ShoesProvider.js
@@ -1,5 +1,17 @@
 import React, { useState, createContext } from "react";
 
+import dylan01 from "../images/boots/dylan01.png";
+import dylan02 from "../images/boots/dylan02.png";
+import dylan03 from "../images/boots/dylan03.png";
+import dylan04 from "../images/boots/dylan04.png";
+import dylan05 from "../images/boots/dylan05.png";
+
+import hargrave from "../images/boots/hargrave.png";
+import cameron from "../images/boots/cameron.png";
+import grayson from "../images/boots/grayson.png";
+import orca from "../images/boots/orca.png";
+import jean from "../images/boots/jean.png";
+
 export const ShoesContext = createContext();
 
 export const ShoesProvider = (props) => {
@@ -9,47 +21,79 @@ export const ShoesProvider = (props) => {
       id: "1",
       brand: "Superbalist",
       price: 500,
-      image: "../images/boots/hargrave.png",
+      sale: true,
+      discount: 40,
+      image: hargrave,
     },
     {
       title: "Cameron Leather",
       id: "2",
       brand: "Superbalist",
       price: 539,
-      image: "../images/boots/cameron.png",
+      lowStock: true,
+      image: cameron,
     },
     {
       title: "Grayson Brogue",
       id: "3",
       brand: "Superbalist",
       price: 420,
-      image: "../images/boots/grayson.png",
+      sale: true,
+      discount: 40,
+      lowStock: true,
+      image: grayson,
     },
     {
       title: "Orca",
       id: "4",
       brand: "Superbalist",
       price: 500,
-      image: "../images/boots/orca.png",
+      sale: true,
+      discount: 40,
+      lowStock: true,
+      image: orca,
     },
     {
       title: "Jean Hiker Boots",
       id: "5",
       brand: "Superbalist",
       price: 600,
-      image: "../images/boots/jean.png",
+      image: jean,
     },
     {
       title: "Dylan Hiker Boot",
       id: "6",
       brand: "Superbalist",
       price: 599,
-      sale: 40,
+      sale: true,
+      discount: 40,
       salePrice: 359,
       color: "Brown",
       fabrication: "Faux Leather",
       details: ["Hiking Boot", "Speed Laces", "Lace-up Design", "Rubber Sole"],
-      image: "../images/boots/dylan01.png",
+
+      images: [
+        {
+          original: dylan01,
+          thumbnail: dylan01,
+        },
+        {
+          original: dylan02,
+          thumbnail: dylan02,
+        },
+        {
+          original: dylan03,
+          thumbnail: dylan03,
+        },
+        {
+          original: dylan04,
+          thumbnail: dylan04,
+        },
+        {
+          original: dylan05,
+          thumbnail: dylan05,
+        },
+      ],
     },
   ]);
 
