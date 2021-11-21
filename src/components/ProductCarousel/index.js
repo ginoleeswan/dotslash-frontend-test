46c17d9765af023289f1dc7c09edeb30b1f845a0
Gino Swanepoel <ginoleemusic@live.com>
2021-11-22 01:17:02 +0200

    styling

--- src/components/ProductCarousel/index.js
+++ src/components/ProductCarousel/index.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container } from "react-bootstrap";
+import ImageGallery from "react-image-gallery";
+import { ShoesContext } from "../../context/ShoesProvider";
 
 const ProductCarousel = () => {
-  return <Container>Product Carousel</Container>;
+  const [boots] = useContext(ShoesContext);
+
+  return (
+    <Container className="productCarouselContainer">
+      <ImageGallery showPlayButton={false} items={boots[5].images} />
+    </Container>
+  );
 };
 
 export default ProductCarousel;
