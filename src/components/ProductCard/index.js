292789ee3b1d21a8a87db6cc6463d19cda8590d8
Gino Swanepoel <ginoleemusic@live.com>
2021-11-22 01:17:29 +0200

    styling, pulls from context api

---
+++ src/components/ProductCard/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { faStoreAlt, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import DiscountTag from "../DiscountTag";
+import LowstockTag from "../LowstockTag";
+
+const ProductCard = ({
+  name,
+  brand,
+  price,
+  image,
+  sale,
+  discount,
+  lowStock,
+}) => {
+  const url = "http://localhost:3000";
+
+  return (
+    <Card
+      style={{
+        width: 240,
+        height: 270,
+        marginRight: 41,
+        marginBottom: 47,
+        paddingLeft: 0,
+        paddingRight: 0,
+        border: "none",
+        borderRadius: 0,
+      }}
+    >
+      <a href="/#" className="cardViewLink">
+        <Card.Img variant="top" alt={name} src={image} />
+        <Container style={{ position: "absolute", left: 106, top: 10 }}>
+          {sale ? <DiscountTag amount={discount} /> : null}
+        </Container>
+        <Container style={{ position: "absolute", left: -13, bottom: 130 }}>
+          {lowStock ? <LowstockTag /> : null}
+        </Container>
+        <Card.Body style={{ padding: 10, paddingTop: 15 }}>
+          <Card.Title className="mb-2" style={{ color: "#111111" }}>
+            <h4 style={{ fontSize: 18 }}>{name}</h4>
+          </Card.Title>
+          <Card.Subtitle className="mb-3">
+            <h5>
+              <FontAwesomeIcon
+                className="productCardStoreIcon"
+                icon={faStoreAlt}
+              />{" "}
+              {brand}
+            </h5>
+          </Card.Subtitle>
+          <Row>
+            <Col style={{ width: "60%", maxWidth: "60%" }}>
+              <Card.Text>
+                <h4 style={{ color: "#00a2ff" }}>R{price}</h4>
+              </Card.Text>
+            </Col>
+            <Col style={{ width: "40%", maxWidth: "40%" }}>
+              <a href="/#" className="cardViewLink">
+                View{" "}
+                <FontAwesomeIcon
+                  className="productCardArrowIcon"
+                  icon={faArrowRight}
+                />
+              </a>
+            </Col>
+          </Row>
+        </Card.Body>
+      </a>
+    </Card>
+  );
+};
+
+export default ProductCard;
